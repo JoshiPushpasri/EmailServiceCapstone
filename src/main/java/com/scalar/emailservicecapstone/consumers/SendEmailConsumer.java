2ca4716bc5498801616f7180f612de4fff38305c
Pushpasri <pushpasrij25@gmail.com>
2026-06-04 04:51:52 +0530

    Adding Email Service and implementing kafka

---
+++ src/main/java/com/scalar/emailservicecapstone/consumers/SendEmailConsumer.java
@@ -0,0 +1,16 @@
+package com.scalar.emailservicecapstone.consumers;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendEmailConsumer
+{
+
+    @KafkaListener(topics = "sendEmail", groupId = "emailService")
+    public void SendEmail(String message)
+    {
+        System.out.println(message);
+    }
+}
---
+++ src/test/java/com/scalar/emailservicecapstone/EmailServiceCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalar.emailservicecapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
